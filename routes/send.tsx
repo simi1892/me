29da21e2e4ffe1455d4cd2aeed5efaf39956df17
Simon Egli <simon.e98zh@gmail.com>
2025-02-09 10:43:30 +0100

    remove log statement for debug deploy

--- routes/send.tsx
+++ routes/send.tsx
@@ -11,7 +11,6 @@ export type ContactForm = {
 };
 
 const NTFY_TOPIC = Deno.env.get("NTFY_TOPIC");
-console.log(Deno.env.toObject())
 if (NTFY_TOPIC === undefined) {
   throw Error("NTFY_TOPIC environment variable must be set");
 }
