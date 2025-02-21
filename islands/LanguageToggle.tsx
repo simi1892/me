863cee4b465f014d7ae8e959ef0a92ace7264832
Simon Egli <simon.e98zh@gmail.com>
2025-02-21 06:53:32 +0100

    add button type

--- islands/LanguageToggle.tsx
+++ islands/LanguageToggle.tsx
@@ -5,6 +5,7 @@ import { State } from "../routes/_middleware.tsx";
 export default function LanguageToggle({ lang }: { lang: State["lang"] }) {
   return (
     <button
+      type='button'
       class="hover:(text-gray-900 dark:text-gray-100) font-bold focus:outline-none"
       onClick={() => {
         document.cookie = `lang=${lang === "en" ? "de" : "en"}`;
